f82695a9857f08a79b5e0bb1b67e0ca692450170
Piyush W <hanumantw99@gmail.com>
2024-06-14 11:25:51 +0530

    Setting up Environment variables and setting up Development phase

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const config = require('config'); // we are using config instead of .env , according to seryian config is better .
 
-mongoose
-    .connect("mongodb://127.0.0.1:27017/scatchUser")
-    .then(function(){
-        console.log("Connected");
+const debug = require('debug')('development:mongoose'); // require('debug')("Anything:Anything") , we can see from where the console.log is comming . This does not work until you set envionment variable . In console ($env:DEBUG = 'development:*') means show all Development related messages. when you dont want to use this (set DEBUG= ).  
+
+
+mongoose.connect(`${config.get("MONGODB_URI")}/scatch`).then(function () {
+        debug("Connected");
     })
     .catch(function (err) {
-        console.log(err.message);
+        debug(err.message);
     });
 
-    module.exports = mongoose.connection;
+module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+const ownerModel = require("../models/owner-model");
+
+router.get('/', (req, res) => {
     res.send("hey")
 });
+
+// To set process.env.NODE_ENV to 'development we console in terminal( $env:NODE_ENV="development" ) to create new envionment varaible
+
+// we use this for development in our local machine, when we push this code to production it will not be related to our machine , and also env varaible would be hide inside our local storage
+if (process.env.NODE_ENV === 'development') {
+    router.post('/create', async (req, res) => {
+        let owners = await ownerModel.find();
+        if (owners.length > 0) {
+            return res.send(500).send("Multiple owners are not allowed !");
+        }
+
+        let { fullname, email, password } = req.body;
+        let createUser = await ownerModel.create({
+            fullname,
+            email,
+            password
+        });
+        res.status(201).send("Owner Created Successfully : "+ createUser);
+    });
+}
 
 module.exports = router;
